561e4ebd29aaeeb820a65abe71dd318435d193d6
Janhavi <jrewdekar@gmail.com>
2024-08-14 16:20:50 +0530

    initial version

---
+++ application.py
@@ -0,0 +1,59 @@
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+app.app_context().push()
+
+
+class Drink(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True,nullable=False)
+    description = db.Column(db.String(120))
+
+    def __repr__(self):
+        return f"{self.name} - {self.description}"
+
+
+@app.route("/")
+def index():
+    return "Hello!!!"
+
+
+@app.route("/drinks")
+def get_drinks():
+    drinks = Drink.query.all()
+    output = []
+    for drink in drinks:
+        drink_data = {'name': drink.name, 'description': drink.description}
+        output.append(drink_data)
+    return {'drinks': output}
+
+@app.route("/drinks/<id>")
+def get_id(id):
+    drink = Drink.query.get_or_404(id)
+    return {"name": drink.name, "description": drink.description}
+
+@app.route("/drinks", methods=['POST'])
+def add_drinks():
+    drink = Drink(name=request.json['name'], description=request.json['description'])
+    db.session.add(drink)
+    db.session.commit()
+    return {"id": drink.id}
+
+
+@app.route("/drinks/<id>", methods=['DELETE'])
+def delete_drinks(id):
+    drink = Drink.query.get(id)
+    if drink is None:
+        return {"error": "not found"}
+    db.session.delete(drink)
+    db.session.commit()
+    return {"message": "successfull"}
+    
+if __name__ == "__main__":
+    app.run(debug=True)
